08f11e30f29d2379713fdad27a75dabf2c856dee
Joseph McCarron <joe@zendesk.com>
2015-01-18 19:40:02 -0800

    refactor, cleanup, add rate purchasing

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 (function() {
-  var f$ = this.jQuery;
-  // dp = new DOMParser();
-  // var oSerializer = new XMLSerializer();
   return {
-    sizes: {
-      'small': {
-        'height': '3',
-        'weight': '5',
-        'width': '5',
-        'length': '5'
-      },
-      'medium': {
-        'height': '6',
-        'weight': '7',
-        'width': '7',
-        'length': '7'
-      },
-      'large': {
-        'height': '12',
-        'weight': '14',
-        'width': '14',
-        'length': '14'
-      }
-    },
     requesterId: null,
     requesterAddress: null,
     requesterCity: null,
@@ -34,23 +11,34 @@
     userNewParams: null,
     confirmed: true,
     productionOn: null,
+    // named events
     events: {
       'app.created':'onAppActivated',
 
       'change #package_size': 'onSizeChanged',
       'change .user-info': 'onUserUpdated',
       'change #ship_type': 'onShipSelected',
-      'click button.initialize': 'init',
+
+      'click button.init_new_shipment': 'initNewShipment',
+      'click button.init_return_shipment': 'initReturnShipment',
+      'click button.init_track_shipment': 'initTrackShipment',
+      'click button.init_validate_address': 'initValidateAddress',
+
       'click .update-decline': 'userUpdateDecline',
       'click .update-user': 'userUpdateConfirm',
       'click .create_address': 'onAddressSubmitted',
       'click .create_shipment': 'onShipmentSubmitted',
+
+      'click .select_rate': 'onRateSelected',
+      'click .buy_rate': 'buyRate',
+
       // Zendesk requests
       'fetchUserFromZendesk.done': 'onUserFetched',
       // EasyPost Requests
       'verifyAddress.done': 'onVerifyAddressDone',
       'createAddress.done': 'onCreateAddressDone',
       'createShipment.done': 'onCreateShipmentDone',
+      'buyRate.done': 'boughtRate'
       
     },
     requests: {
@@ -61,12 +49,7 @@
       },
       // easypost requests
       createAddress: function(data) {
-        var token;
-        if (this.setting('production_on') === true) {
-          token = btoa(this.setting('easypost_production_token') + ":");
-        } else {
-          token = btoa(this.setting('easypost_testing_token') + ":");
-        }
+        var token = this.token();
         return {
           url: 'https://api.easypost.com/v2/addresses',
           type: 'POST',
@@ -76,31 +59,33 @@
         };
       },
       verifyAddress: function(id) {
-        var token;
-        if (this.setting('production_on') === true) {
-          token = btoa(this.setting('easypost_production_token') + ":");
-        } else {
-          token = btoa(this.setting('easypost_testing_token') + ":");
-        }
+        var token = this.token();
         return {
           url: 'https://api.easypost.com/v2/addresses/' + id + '/verify',
           headers: {"Authorization": "Basic " + token}
         };
       },
-
       createShipment: function (data) {
-        var token;
-        if (this.setting('production_on') === true) {
-          token = btoa(this.setting('easypost_production_token') + ":");
-        } else {
-          token = btoa(this.setting('easypost_testing_token') + ":");
-        }
+        var token = this.token();
         return {
           url: 'https://api.easypost.com/v2/shipments',
           type: 'POST',
           data: data,
-          headers: {"Authorization": "Basic " + btoa(this.setting('easypost_testing_token') + ":")}
+          headers: {"Authorization": "Basic " + token}
           // secure: true
+        };
+      },
+      buyRate: function(rateID, shipmentID) {
+        var token = this.token();
+        return {
+          url: 'https://api.easypost.com/v2/shipments/' + shipmentID + '/buy',
+          type: 'POST',
+          data: {
+            rate: {
+              id: rateID
+            }
+          },
+          headers: {"Authorization": "Basic " + token}
         };
       },
       
@@ -135,42 +120,60 @@
     },
 
     onAppActivated: function(app) {
+      // assign global variables
       if (this.setting('editable_form') === true) {
         this.editableForm = true;
       }
       if (this.setting('production_on') === true) {
         this.productionOn = true;
       }
-
       this.requesterId = this.ticket().requester().id();
       this.setUpSizes();
+
+      // show home page
+      this.switchTo('home');
+    },
+    // init: function(e) {
+    //   if(e) {e.preventDefault();}
+    //   this.showForm();
+    // },
+    initNewShipment: function(e) {
+      if(e) {e.preventDefault();}
       this.showForm();
+    },
+    initReturnShipment: function(e) {
+      if(e) {e.preventDefault();}
+      
+    },
+    initValidateAddress: function(e) {
+      if(e) {e.preventDefault();}
+      
+    },
+    initTrackShipment: function(e) {
+      if(e) {e.preventDefault();}
+
     },
     setUpSizes: function(){
       this.sizes = {
         'small': {
-          'height': this.setting('small_size_height'),
-          'weight': this.setting('small_size_weight'),
-          'width': this.setting('small_size_width'),
-          'length': this.setting('small_size_length')
+          'height': this.setting('small_size_height') || 3,
+          'weight': this.setting('small_size_weight') || 5,
+          'width': this.setting('small_size_width') || 5,
+          'length': this.setting('small_size_length') || 5
         },
         'medium': {
-          'height': this.setting('medium_size_height'),
-          'weight': this.setting('medium_size_weight'),
-          'width': this.setting('medium_size_width'),
-          'length': this.setting('medium_size_length')
+          'height': this.setting('medium_size_height') || 6,
+          'weight': this.setting('medium_size_weight') || 7,
+          'width': this.setting('medium_size_width') || 7,
+          'length': this.setting('medium_size_length' || 7)
         },
         'large': {
-          'height': this.setting('large_size_height'),
-          'weight': this.setting('large_size_weight'),
-          'width': this.setting('large_size_width'),
-          'length': this.setting('large_size_length')
+          'height': this.setting('large_size_height') || 12,
+          'weight': this.setting('large_size_weight') || 14,
+          'width': this.setting('large_size_width') || 14,
+          'length': this.setting('large_size_length') || 14
         }
       };
-    },
-    init: function(e) {
-      if(e) {e.preventDefault();}
-      this.showForm();
     },
     showForm: function(response) {
       if(response) {
@@ -186,10 +189,11 @@
             "show": this.editableForm,
             "message": response.message
           });
-          this.$('.create_shipment').prop('disabled', true);
+          // this.$('.create_shipment').prop('disabled', true);
         }
-        this.setUpShipToForm(response.address);
+        this.setUpShipToForm(response.address); // fills in form with validated address details
       } else {
+        // if no address is supplied just grab the user address from their profile
         this.ajax('fetchUserFromZendesk');
         this.switchTo('form', {
           "show": this.editableForm
@@ -200,6 +204,7 @@
     },
     setUpShipToForm: function(address) {
       if(address) {
+        // if an address is passed, use that to fill in the destination form
         this.$('input[name=name]').val(address.name);
         this.$('input[name=address]').val(address.street1);
         this.$('input[name=city]').val(address.city);
@@ -207,6 +212,7 @@
         this.$('input[name=zip_code]').val(address.zip);
         this.$('input[name=country]').val(address.country);
       }
+      // fill in the origin form (conditionally shown)
       this.$('input[name=origin_name]').val(this.setting("company_name"));
       this.$('input[name=origin_address]').val(this.setting("business_address"));
       this.$('input[name=origin_city]').val(this.setting("city"));
@@ -215,47 +221,31 @@
       this.$('input[name=origin_country]').val(this.setting("country_code"));
       
     },
-    onAddressSubmitted: function(e) {
+    onAddressSubmitted: function(e) { // validate the address
       if (e) { e.preventDefault(); }
-      // if (this.userNewParams) {
-      //   this.showUpdateUserOption();
-      //   this.confirmed = false;
-      //   return false;
-      // }
-      var address = {};
-      // to address
-      address.name = this.$('input[name=name]').val() || this.setting('company_name');
-      address.street1 = this.$('input[name=address]').val() || this.setting('business_address');
-      address.city = this.$('input[name=city]').val() || this.setting('city');
-      address.state = this.$('input[name=state]').val() || this.setting('state').toUpperCase();
-      address.country = this.$('input[name=country]').val() || this.setting('country_code').toUpperCase();
-      address.zip = this.$('input[name=zip_code]').val() || this.setting('zip_code');
-
-      for (var key in address) {
-        if (!address[key]) {
-          services.notify('Please fill in the field for "' + key + '" before continuing.');
-          return false;
-        }
-      }
+      // destination address
+      var address = this.toAddress();
+      // client-side validation
       if (address.state.length > 2) { services.notify("Please use the 2-letter code for State or Province before submitting"); return;}
-      this.switchTo('loading');
-      var data = {
+      // construct the payload & send to EasyPost
+      var data;
+      this.ajax('createAddress', {
         "address": address
-      };
-      this.ajax('createAddress', data);
+      });
+      this.switchTo('loading');
     },
     onCreateAddressDone: function(response) {
       var addressID = response.id;
+      // validate the address w/ EasyPost (by ID)
       this.ajax('verifyAddress', addressID);
-      // console.log(response);
-      // this.switchTo('button');
     },
     onVerifyAddressDone: function(response) {
       this.showForm(response);
     },
     onShipmentSubmitted: function(e) {
+      // when creating a shipment
       if (e) { e.preventDefault(); }
-      if (this.userNewParams) {
+      if (this.userNewParams) { // if the user's info doesn't match what is on their profile ask if the agent wants to update it
         this.showUpdateUserOption();
         this.confirmed = false;
         return false;
@@ -265,33 +255,38 @@
         "to_address": {},
         "parcel": {}
       };
-      // from address
-      shipment.from_address.name = this.$('input[name=name]').val();
-      shipment.from_address.street1 = this.$('input[name=address]').val();
-      shipment.from_address.city = this.$('input[name=city]').val();
-      shipment.from_address.country = this.$('input[name=country]').val().toUpperCase().substring(0, 2);
-      shipment.from_address.state = this.$('input[name=state]').val().toUpperCase();
-      if ( this.$('input[name=zip_code]').val().length > 0 ){
-        shipment.from_address.zip = this.$('input[name=zip_code]').val().match(/[a-z0-9]/ig).join("");
+    // get destination address
+      shipment.to_address = this.toAddress();
+      // if ( this.$('input[name=zip_code]').val().length > 0 ){
+      //   shipment.from_address.zip = this.$('input[name=zip_code]').val().match(/[a-z0-9]/ig).join("");
+      // }
+    // get origin address
+      shipment.from_address = this.fromAddress();
+    // get parcel info
+      var packageSize = this.$('select#package_size').val();
+      var dimensions;
+      if (packageSize == 'custom') {
+        // get the dimensions from the in=app form
+        dimensions = {
+          'height': this.$('input[name=height]'),
+          'weight': this.$('input[name=weight]'),
+          'width': this.$('input[name=width]'),
+          'length': this.$('input[name=length')
+        };
+      } else {
+        // get the dimensions for the selected size from Settings
+        dimensions = this.sizes[ packageSize ];
       }
-      shipment.from_address.email = this.$('input[name=email]').val();
+      // TODO add options for predefined packages
 
-      // to address
-      shipment.to_address.name = this.$('input[name=origin_name]').val() || this.setting('company_name');
-      shipment.to_address.street1 = this.$('input[name=origin_address]').val() || this.setting('business_address');
-      shipment.to_address.city = this.$('input[name=origin_city]').val() || this.setting('city');
-      shipment.to_address.state = this.$('input[name=origin_state]').val() || this.setting('state').toUpperCase();
-      shipment.to_address.country = this.$('input[name=origin_country]').val() || this.setting('country_code').toUpperCase();
-      shipment.to_address.zip = this.$('input[name=origin_zip_code]').val() || this.setting('zip_code');
-
-      // shipment.psize = this.sizes[this.$('select#package_size').val()];
-      var dimensions = this.sizes[this.$('select#package_size').val()];
-      // shipment.parcel.length = dimensions.length;
-      // shipment.parcel.height = dimensions.height;
-      // shipment.parcel.width = dimensions.width;
-      shipment.parcel.weight = dimensions.weight;
-      shipment.parcel.predefined_package = 'LargeFlatRateBox';
-
+      // built parcel data for EasyPost
+      shipment.parcel = {
+        length: dimensions.length,
+        height: dimensions.height,
+        width:  dimensions.width,
+        weight: dimensions.weight
+      };
+      // validate client-side
       for (var key in shipment) {
         if (!shipment[key]) {
           services.notify('Please fill in the field for "' + key + '" before continuing.');
@@ -307,11 +302,38 @@
     },
     onCreateShipmentDone: function(response) {
       console.log(response.rates);
+      this.rates = response.rates;
+      this.shipmentID = response.id;
       this.switchTo('rates', {
         "rates":response.rates
       });
     },
-    onShipSelected: function(e) {
+    onRateSelected: function(e) {
+      e.preventDefault();
+      console.log(e.currentTarget.dataset.rateId);
+      var rateId = e.currentTarget.dataset.rateId;
+      var rate = _.find(this.rates, function(rate) {
+        return rateId == rate.id;
+      });
+      this.switchTo('selectedRate', {
+        rate: rate
+      });
+    },
+    buyRate: function(e) {
+      e.preventDefault();
+      var rateID = e.currentTarget.dataset.rateId;
+      this.ajax('buyRate', rateID, this.shipmentID);
+    },
+    boughtRate: function(response) {
+      console.log(response);
+      this.updateTicket(response);
+      this.switchTo('createdShipment', {
+        response: response
+      });
+    },
+
+
+    onShipSelected: function(e) { // ??
       if (this.$(e.target).val() == "12") {
         this.$('.valueBox').show();
       } else {
@@ -319,6 +341,11 @@
       }
     },
 
+    updateTicket: function(response) {
+      var url = response.postage_label.label_url;
+      var comment = helpers.fmt("Label URL: %@ [ ![Shipping Label](%@) ](%@)", url, url, url);
+      this.ajax('updateTicketComment', comment, response.tracking_code);
+    },
 
     showUpdateUserOption: function() {
       this.$('.update-confirm').fadeIn();
@@ -341,6 +368,8 @@
         }
       }
     },
+
+
     onSizeChanged: function(event) {
       var sizeSelected = this.$(event.target).val();
       if (sizeSelected == 'custom') {
@@ -360,8 +389,8 @@
 
     // User updates
     onUserUpdated: function(e) {
-       var self = this,
-          newVal = this.$(e.target).val();
+      var self = this,
+        newVal = this.$(e.target).val();
       if (!this.userNewParams) { this.userNewParams = {}; }
       switch( this.$(e.target).attr('name')) {
         case 'name':
@@ -386,17 +415,48 @@
     },
       // TODO: dry up these two
     userUpdateConfirm: function(e) {
+      e.preventDefault();
       this.ajax('updateUser');
       this.$('#update-confirm').fadeOut();
       this.userNewParams = null;
       this.onShipmentSubmitted();
-      e.preventDefault();
     },
     userUpdateDecline: function(e) {
+      e.preventDefault();
       this.$('.update-confirm').fadeOut();
       this.userNewParams = null;
       this.onShipmentSubmitted();
-      e.preventDefault();
+    },
+    // 
+    token: function() {
+      if (this.setting('production_on') === true) {
+        return btoa(this.setting('easypost_production_token') + ":");
+      } else {
+        return btoa(this.setting('easypost_testing_token') + ":");
+      }
+    },
+    toAddress: function() {
+      return {
+        name:     this.$('input[name=name]').val(),
+        street1:  this.$('input[name=address]').val(),
+        city:     this.$('input[name=city]').val(),
+        state:    this.$('input[name=state]').val().toUpperCase(),
+        country:  this.$('input[name=country]').val().toUpperCase().substring(0, 2),
+        zip:      this.$('input[name=zip_code]').val().match(/[a-z0-9]/ig).join(""),
+        email:    this.$('input[name=email]').val()
+      };
+    },
+    fromAddress: function() {
+      return {
+        name:     this.$('input[name=origin_name]').val() || this.setting('company_name'),
+        street1:  this.$('input[name=origin_address]').val() || this.setting('business_address'),
+        city:     this.$('input[name=origin_city]').val() || this.setting('city'),
+        state:    this.$('input[name=origin_state]').val() || this.setting('state').toUpperCase(),
+        country:  this.$('input[name=origin_country]').val() || this.setting('country_code').toUpperCase(),
+        zip:      this.$('input[name=origin_zip_code]').val() || this.setting('zip_code'),
+        email:    this.$('input[name=origin_email]').val() || this.setting('support_email'),
+        phone:    this.$('input[name=origin_phone]').val() || this.setting('phone_number')
+      };
     },
 
   // --------- UTILITY FUNCTIONS --------- //
